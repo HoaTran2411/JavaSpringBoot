8a912999a678bce73855e9f745e6839bbb6aefd3
HoaTran2411 <hoatrantcnh@gmail.com>
2021-09-17 12:56:21 +0700

    upload BTVN

---
+++ bookstore/src/main/java/vn/techmaster/bookstore/controller/RESTController.java
@@ -0,0 +1,61 @@
+package vn.techmaster.bookstore.controller;
+
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+import vn.techmaster.bookstore.model.Book;
+import vn.techmaster.bookstore.repository.BookDao;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+
+public class RESTController {
+
+    BookDao bookDao = new BookDao();
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> listBooks() {
+        return ResponseEntity.ok().body(bookDao.getAll());
+    }
+
+    // Xem chi tiết một đầu sách theo id
+    @GetMapping("/id")
+    public ResponseEntity<Optional<Book>> getBookById(@RequestParam(value = "id") int id) {
+        BookDao bookDao = new BookDao();
+        return ResponseEntity.ok().body(bookDao.get(id));
+    }
+
+    // Thêm mới một đầu sách
+    @PostMapping("/booksAfterAdd")
+    public ResponseEntity<List<Book>> addBooks(@RequestParam(value = "id") int id,
+            @RequestParam(value = "title") String title, @RequestParam(value = "description") String description) {
+        bookDao.add(new Book(id, title, description));
+        return ResponseEntity.ok().body(bookDao.getAll());
+    }
+
+    // Cập nhật một đầu sách theo id
+    @PutMapping("/booksAfterUpdate")
+    public ResponseEntity<List<Book>> updateBook(@RequestParam(value = "id") int id,
+            @RequestParam(value = "title") String title, @RequestParam(value = "description") String description) {
+        bookDao.update(id, new Book(id, title, description));
+        return ResponseEntity.ok().body(bookDao.getAll());
+    }
+
+    // Xóa một đầu sách theo id
+    @DeleteMapping("/booksAfterDelete")
+    public ResponseEntity<List<Book>> deleteBook(@RequestParam(value = "id") int id) {
+        bookDao.deleteByID(id);
+        return ResponseEntity.ok().body(bookDao.getAll());
+    }
+
+}
